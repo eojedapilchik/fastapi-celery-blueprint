8069bf8055b1145728a3ed15f65ecf4b49f5cc08
Alcides Ojeda <alcides.ojeda@email.com>
2023-03-17 15:27:21 +0100

    Add celery [boilerplate] remove log duplicates

--- logging_config.py
+++ logging_config.py
@@ -6,16 +6,22 @@ import os
 
 load_dotenv()  # load the environment variables from .env file
 
+loggers = {}
 
 
 def get_logger():
+    # if loggers.get("webservice"):
+        # return loggers.get("webservice")
     log_file = os.getenv("LOG_FILE_LOCATION")
     logger = logging.getLogger('webservice')
+    if logger.hasHandlers():
+        logger.handlers.clear()
     logger.setLevel(logging.DEBUG)
-
     formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(name)s: %(message)s')
     handler = logging.handlers.RotatingFileHandler(log_file, maxBytes=10 * 1024 * 1024, backupCount=5)
     handler.setFormatter(formatter)
-
+    logger.propagate = False
     logger.addHandler(handler)
+    # logging.getLogger("webservice").propagate = False
+    # loggers["webservice"] = logger
     return logger
