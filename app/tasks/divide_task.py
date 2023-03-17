544cb02d3afd2f7654456eacdd9b67ca9085956b
Alcides Ojeda <alcides.ojeda@email.com>
2023-03-17 16:37:39 +0100

    refactor  for handling error

--- app/tasks/divide_task.py
+++ app/tasks/divide_task.py
@@ -6,11 +6,15 @@ from logging_config import get_logger
 logger = get_logger()
 
 
-@celery_app.task
-def divide_task(a: float, b: float):
+@celery_app.task(bind=True, max_retries=3)
+def divide_task(self, a: float, b: float):
     logger.info("Processing tasks divide_task...")
     print("Processing tasks divide_task...")
     time.sleep(20)
-    result = a / b
-    logger.info(f"Result of dividing {a} by {b}: {result}")
+    try:
+        result = a / b
+        logger.info(f"Result of dividing {a} by {b}: {result}")
+    except ZeroDivisionError as e:
+        # If the error is recoverable, retry the task
+        raise self.retry(exc=e)
     return result
